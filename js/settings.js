2333b4644a2d3dc901a16706d9e9c30cb65575ea
Klaas <KlaasNe@users.noreply.github.com>
2023-12-31 16:58:23 +0100

    Working on the settings screen

---
+++ js/settings.js
@@ -0,0 +1,5 @@
+function add_product() {
+    const product_name = document.getElementById("product").value
+    const price = document.getElementById("price").value
+    console.log(product_name, price)
+}
